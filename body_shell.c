5061b95f16a0e032b6b68cf0f02ee9e184f107c6
Saniez-l <saniezl.christophe@gmail.com>
2025-08-17 21:42:23 +0200

    change the name of file corp_of_shell to body_shell

---
+++ body_shell.c
@@ -0,0 +1,30 @@
+#include "header.h"
+/**
+ * shell - program for print $ and enter command user
+ * @argv: pointeur of array argumend ask by user
+ * Return: 0
+ */
+int shell(char *argv[])
+
+{
+	char *buff = NULL, *next;
+	size_t i = 0, n = 0;
+
+	printf("$ ");
+	if (getline(&buff, &n, stdin) == -1)
+	{
+		free(buff);
+		return (1);
+	}
+	next = strtok(buff, " \t\n");
+	while (next != NULL)
+	{
+		argv[i] = next;
+		i++;
+		next = strtok(NULL, " \t\n");
+	}
+	argv[i] = NULL;
+	shell_fork(argv);
+	free(buff);
+	return (0);
+}
