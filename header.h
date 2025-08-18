997b76821c20303b94365420c2d81c0c9867574c
chermat mustapha <mchermat@outlook.fr>
2025-08-18 20:22:36 +0200

    add prototype off free function

--- header.h
+++ header.h
@@ -1,5 +1,6 @@
 #ifndef HEADER_H
 #define HEADER_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -7,9 +8,30 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <dirent.h>
+#include <stddef.h>
+#include <fcntl.h>
 
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+	const char *str;
+	struct list_s *next;
+} list_t;
+
+list_t *add_node(list_t **head, const char *str);
+char *_getenv(const char *name);
+list_t *list_env(void);
 extern char **environ;
 int shell(char *argv[]);
 int shell_fork(char *argv[]);
-int _print_env(int i);
+int _print_env(void);
+void free_list(list_t *head);
+
 #endif
