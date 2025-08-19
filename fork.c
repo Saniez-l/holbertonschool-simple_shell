6bb840470b8678a31137e9118816885423e87bad
chermat mustapha <mchermat@outlook.fr>
2025-08-19 15:18:15 +0200

    create new function for the PATH and add condition int the fork function

--- fork.c
+++ fork.c
@@ -1,11 +1,64 @@
 #include "header.h"
 
 /**
+*exe_path -  execute a path commande
+*@argv: the commande to execute
+*@head: the list with the PATH directories
+*Return: always 0
+*/
+int exe_path(char *argv[], list_t *head)
+{
+	DIR *op = NULL;
+	struct dirent *_read = NULL;
+	char buffer[64];
+	int i, j, len = 0, len2 = 0;
+
+	if (head == NULL)
+		return (0);
+	while (head != NULL)
+	{
+		op = opendir(head->str);
+		if (op == NULL)
+		{
+			head = head->next;
+			continue; }
+		while ((_read = readdir(op)) != NULL)
+		{
+			if ((strcmp(_read->d_name, argv[0]) == 0) && head->str[0] == '/')
+			{
+				i = 0;
+				j = 0;
+				len = strlen(head->str);
+				len2 = strlen(_read->d_name);
+				while (i < len)
+				{
+					buffer[i] = head->str[i];
+					i++; }
+				if (i > 0 && buffer[i - 1] != '/')
+				{
+					buffer[i] = '/';
+					i++; }
+				while (i < (len + len2 + 1))
+				{
+					buffer[i] = _read->d_name[j];
+					i++;
+					j++; }
+				buffer[i] = '\0';
+				execve (buffer, argv, environ); }
+		}
+		closedir(op); 
+		head = head->next; }
+	closedir(op);
+	return (0);
+}
+
+/**
  * shell_fork - function fork shell
  * @argv: pointeur of array argumend ask by user
+ *@head: a list with all path's directories
  * Return: 0
  */
-int shell_fork(char *argv[])
+int shell_fork(char *argv[], list_t *head)
 {
 	pid_t id;
 
@@ -19,7 +72,13 @@ int shell_fork(char *argv[])
 	{
 		if (strcmp(argv[0], "env") == 0)
 		{
-			_print_env(0);
+			_print_env();
+			exit(1);
+		}
+		else if (head != NULL && *argv[0] != '/')
+		{
+			exe_path(argv, head);
+			perror("exepath");
 			exit(1);
 		}
 		else
